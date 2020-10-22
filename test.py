db2522140f61d839095ad14d787aa580b17d6b39
dlftls38 <dlftls38@naver.com>
2020-10-23 00:09:44 +0900

    dasf

--- test.py
+++ test.py
@@ -1,21 +1,2 @@
 fdsh ajkf hsadjklf hasdjkfh jkasd hasdjkfh
 dsafj kasdhjfk sa
-432
-fdsja kfhkasdj for 
-fhsdja fjksa
-fdasfsdafhs
-safsajkf  in expression_list:
-    pass
-dsafh ksdja
-
-dfsjah fadshkjf sdaf 
-fdaskjfhsdka
-fdsafas
-4
-fdshajkf hdsjakl fh 
-fdsaj fdshajkf12
-df safhk
-4
-fhdjsakf hkjsda hfjklsadhjfk234sd234214
-4
-323
